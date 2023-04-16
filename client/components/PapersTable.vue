6837e22a50911b93b25a6e4b318f3225b0916163
Mitch J <mjacovet@kent.edu>
2023-04-15 20:51:28 -0400

    handle authors more carefully

--- client/components/PapersTable.vue
+++ client/components/PapersTable.vue
@@ -46,13 +46,14 @@ const cancel = (row: Paper) => {
 }
 const cleanAuthors = (authors: AuthorName[]) => {
   for (let i = 0; i < authors.length; ++i) {
+    if (!authors[i].lastName || authors[i].lastName.trim().length < 0) {
+      authors.splice(i, 1);
+      --i;
+      continue;
+    }
     authors[i].prefix = authors[i].prefix.trim();
     authors[i].lastName = authors[i].lastName.trim();
     authors[i].suffix = authors[i].suffix.trim();
-    if (authors[i].lastName.trim().length < 0) {
-      authors.splice(i, 1);
-      --i;
-    }
   }
   return authors;
 }
