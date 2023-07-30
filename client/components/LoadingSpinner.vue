1cc823d905e07168d7cd751f60b7ad54d1526a8d
Mitch J <mjacovet@kent.edu>
2023-07-30 18:36:16 -0400

    better loading spinner for pdfs

--- client/components/LoadingSpinner.vue
+++ client/components/LoadingSpinner.vue
@@ -1,78 +1,38 @@
 <template>
-    <div>
-        <div class="lds-ellipsis">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        style="margin: auto; background: white; display: block;" width="208px" height="208px" viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid">
+        <rect x="13" y="13" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="38" y="13" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.125s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="63" y="13" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.25s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="13" y="38" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.875s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="63" y="38" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.375s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="13" y="63" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.75s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="38" y="63" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.625s" calcMode="discrete"></animate>
+        </rect>
+        <rect x="63" y="63" width="24" height="24" fill="#97c9e2">
+            <animate attributeName="fill" values="#a5daa8;#97c9e2;#97c9e2" keyTimes="0;0.125;1" dur="1s"
+                repeatCount="indefinite" begin="0.5s" calcMode="discrete"></animate>
+        </rect>
+    </svg>
 </template>
-<style scoped>
-.lds-ellipsis {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-}
-
-.lds-ellipsis div {
-    position: absolute;
-    top: 33px;
-    width: 13px;
-    height: 13px;
-    border-radius: 50%;
-    background: rgb(244, 51, 244);
-    animation-timing-function: cubic-bezier(0, 1, 1, 0);
-}
-
-.lds-ellipsis div:nth-child(1) {
-    left: 8px;
-    animation: lds-ellipsis1 0.6s infinite;
-}
-
-.lds-ellipsis div:nth-child(2) {
-    left: 8px;
-    animation: lds-ellipsis2 0.6s infinite;
-}
-
-.lds-ellipsis div:nth-child(3) {
-    left: 32px;
-    animation: lds-ellipsis2 0.6s infinite;
-}
-
-.lds-ellipsis div:nth-child(4) {
-    left: 56px;
-    animation: lds-ellipsis3 0.6s infinite;
-}
-
-@keyframes lds-ellipsis1 {
-    0% {
-        transform: scale(0);
-    }
-
-    100% {
-        transform: scale(1);
-    }
-}
-
-@keyframes lds-ellipsis3 {
-    0% {
-        transform: scale(1);
-    }
-
-    100% {
-        transform: scale(0);
-    }
-}
-
-@keyframes lds-ellipsis2 {
-    0% {
-        transform: translate(0, 0);
-    }
-
-    100% {
-        transform: translate(24px, 0);
-    }
-}
-</style>
