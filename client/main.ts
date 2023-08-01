12a2e4fc69b0a63d019808e9f32ca64aca788796
Mitch J <mjacovet@kent.edu>
2023-08-01 02:55:43 -0400

    code-split NoteTaker out of main bundle

--- client/main.ts
+++ client/main.ts
@@ -3,11 +3,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 import SmartTable from "vuejs-smart-table";
 import PapersTable from "./components/PapersTable.vue";
-import NoteTaker from "./components/NoteTaker.vue";
 
 const routes = [
   { path: "/", component: PapersTable },
-  { path: "/notes/:id", component: NoteTaker },
+  { path: "/notes/:id", component: () => import("./components/NoteTaker.vue") },
 ];
 
 createApp(App)
